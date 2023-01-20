84dfda7a8bf2e0a189ad0c4b880f4b4fe40142ad
momotsow <kgomotsomkansi@gmail.com>
2023-01-20 17:30:31 +0200

    Add data

--- schema.sql
+++ schema.sql
@@ -1,7 +1,6 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE TABLE animals (
-    CREATE TABLE animal (
     id BIGINT NOT NULL PRIMARY KEY,
     name VARCHAR (100),
     date_of_birth DATE,
